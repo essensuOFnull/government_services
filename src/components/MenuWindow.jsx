d3288c09ad70270074c83228492f6c0719b52957
essensuOFnull <essensuOFnull@gmail.com>
2026-01-24 21:47:53 +0500

    немного исправлены списки во вкладке "Организации", добавлена заготовка вкладки с калькуляторами с пока пустым окном калькулатора разбора декора Crossout

--- src/components/MenuWindow.jsx
+++ src/components/MenuWindow.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Messenger from './Messenger';
 import { useWindowsManager } from '../hooks/useWindowsManager';
 import { useAuth } from '../hooks/useAuth';
+import DecorDisassembling from './calculators/crossout/DecorDisassembling';
 
 export default function MenuWindow({ userId }) {
 	const [activeTab, setActiveTab] = useState(0);
@@ -41,6 +42,7 @@ export default function MenuWindow({ userId }) {
 				<li role="tab" aria-selected={activeTab === 0} onClick={() => setActiveTab(0)}><a>Мессенджер</a></li>
 				<li role="tab" aria-selected={activeTab === 1} onClick={() => setActiveTab(1)}><a>Хранилище</a></li>
 				<li role="tab" aria-selected={activeTab === 2} onClick={() => setActiveTab(2)}><a>Организации</a></li>
+				<li role="tab" aria-selected={activeTab === 2} onClick={() => setActiveTab(2)}><a>Калькуляторы</a></li>
 			</menu>
 			<div className="window" role="tabpanel" style={{ display: activeTab === 0 ? 'block' : 'none' }}>
 				<div className="window-body">
@@ -64,176 +66,220 @@ export default function MenuWindow({ userId }) {
 			<div className="window" role="tabpanel" style={{ display: activeTab === 2 ? 'block' : 'none' }}>
 				<div className="window-body">
 					<ul className="tree-view">
-						<details open>
-							<summary>Список услуг:</summary>
-							<ul>
-								<details>
-									<summary>ПРКПН (ПодбредРосКомПозорНадзор)</summary>
-									<ul>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Мне нужна консультация по поводу обхода блокировок. Каким образом происходит данный процесс?',()=>{
-													window.open(ПРКПН_link, '_blank');
-												});
-											}}>Запросить консультацию по обходу блокировок</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Мне нужен текущий общий конфигурационный файл для обхода блокировок, а также приложение для его запуска.',()=>{
-													window.open(ПРКПН_link, '_blank');
-												});
-											}}>Получить текущий сбособ обхода блокировок</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Мне нужен новый конфигурационный файл для обхода блокировок.',()=>{
-													window.open(ПРКПН_link, '_blank');
-												});
-											}}>Запросить новый конфиг</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard(other,()=>{
-													window.open(ПРКПН_link, '_blank');
-												});
-											}}>Обращение по другому поводу</button>
-										</li>
-									</ul>
-								</details>
-								<details>
-									<summary>SRDI (Steal-ReDistribute-Internet)</summary>
-									<ul>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Научите меня взламывать wifi.',()=>{
-													window.open(SRDI_link, '_blank');
-												});
-											}}>Запросить консультацию по взлому wifi</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Мне нужно оборудовние для захвата хендшейков.',()=>{
-													window.open(SRDI_link, '_blank');
-												});
-											}}>Запросить оборудование для захвата хендшейков</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Мне нужны вычислительные мощности для брутфоса.',()=>{
-													window.open(SRDI_link, '_blank');
-												});
-											}}>Запросить брутфорс хендшейка</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard(other,()=>{
-													window.open(SRDI_link, '_blank');
-												});
-											}}>Обращение по другому поводу</button>
-										</li>
-									</ul>
-								</details>
-								<details>
-									<summary>ОМГ (Отдел Майнинга и Генерации)</summary>
-									<ul>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Научите меня программировать.',()=>{
-													window.open(ОМГ_link, '_blank');
-												});
-											}}>Запросить консультацию по программированию</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Создайте программу для',()=>{
-													window.open(ОМГ_link, '_blank');
-												});
-											}}>Запросить создание программы</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Модифицируйте существующую программу',()=>{
-													window.open(ОМГ_link, '_blank');
-												});
-											}}>Запросить модификацию программы</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard(other,()=>{
-													window.open(ОМГ_link, '_blank');
-												});
-											}}>Обращение по другому поводу</button>
-										</li>
-									</ul>
-								</details>
-								<details>
-									<summary>ОСОС (Отдел Сервисного Обслуживания Сети)</summary>
-									<ul>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Как мне сделать так, чтобы друг смог зайти на мой сервер?',()=>{
-													window.open(ОСОС_link, '_blank');
-												});
-											}}>Запросить консультацию по предоставлению доступа к игровому серверу</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Как мне открыть порт?',()=>{
-													window.open(ОСОС_link, '_blank');
-												});
-											}}>Запросить консультацию по открытию порта</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Как мне создать виртуальную локальную сеть?',()=>{
-													window.open(ОСОС_link, '_blank');
-												});
-											}}>Запросить консультацию по созданию виртуальной локальной сети</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard(other,()=>{
-													window.open(ОСОС_link, '_blank');
-												});
-											}}>Обращение по другому поводу</button>
-										</li>
-									</ul>
-								</details>
-								<details>
-									<summary>ХВЗ (Хранилище Вечного Знания)</summary>
-									<ul>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Мне нужна книга.',()=>{
-													window.open(ХВЗ_link, '_blank');
-												});
-											}}>Запрос на предоставление доступа к книге</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Посоветуйте книгу.',()=>{
-													window.open(ХВЗ_link, '_blank');
-												});
-											}}>Рекомендация книги по запросу</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard('Посоветуйте случайную книгу.',()=>{
-													window.open(ХВЗ_link, '_blank');
-												});
-											}}>Рекомендация случайной книги</button>
-										</li>
-										<li>
-											<button onClick={()=>{
-												copyToClipboard(other,()=>{
-													window.open(ХВЗ_link, '_blank');
-												});
-											}}>Обращение по другому поводу</button>
-										</li>
-									</ul>
-								</details>
-							</ul>
-						</details>
+						<li>
+							<details open>
+								<summary>Список услуг:</summary>
+								<ul>
+									<li>
+										<details>
+											<summary>ПРКПН (ПодбредРосКомПозорНадзор)</summary>
+											<ul>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Мне нужна консультация по поводу обхода блокировок. Каким образом происходит данный процесс?',()=>{
+															window.open(ПРКПН_link, '_blank');
+														});
+													}}>Запросить консультацию по обходу блокировок</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Мне нужен текущий общий конфигурационный файл для обхода блокировок, а также приложение для его запуска.',()=>{
+															window.open(ПРКПН_link, '_blank');
+														});
+													}}>Получить текущий сбособ обхода блокировок</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Мне нужен новый конфигурационный файл для обхода блокировок.',()=>{
+															window.open(ПРКПН_link, '_blank');
+														});
+													}}>Запросить новый конфиг</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard(other,()=>{
+															window.open(ПРКПН_link, '_blank');
+														});
+													}}>Обращение по другому поводу</button>
+												</li>
+											</ul>
+										</details>
+									</li>
+									<li>
+										<details>
+											<summary>SRDI (Steal-ReDistribute-Internet)</summary>
+											<ul>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Научите меня взламывать wifi.',()=>{
+															window.open(SRDI_link, '_blank');
+														});
+													}}>Запросить консультацию по взлому wifi</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Мне нужно оборудовние для захвата хендшейков.',()=>{
+															window.open(SRDI_link, '_blank');
+														});
+													}}>Запросить оборудование для захвата хендшейков</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Мне нужны вычислительные мощности для брутфоса.',()=>{
+															window.open(SRDI_link, '_blank');
+														});
+													}}>Запросить брутфорс хендшейка</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard(other,()=>{
+															window.open(SRDI_link, '_blank');
+														});
+													}}>Обращение по другому поводу</button>
+												</li>
+											</ul>
+										</details>
+									</li>
+									<li>
+										<details>
+											<summary>ОМГ (Отдел Майнинга и Генерации)</summary>
+											<ul>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Научите меня программировать.',()=>{
+															window.open(ОМГ_link, '_blank');
+														});
+													}}>Запросить консультацию по программированию</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Создайте программу для',()=>{
+															window.open(ОМГ_link, '_blank');
+														});
+													}}>Запросить создание программы</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Модифицируйте существующую программу',()=>{
+															window.open(ОМГ_link, '_blank');
+														});
+													}}>Запросить модификацию программы</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard(other,()=>{
+															window.open(ОМГ_link, '_blank');
+														});
+													}}>Обращение по другому поводу</button>
+												</li>
+											</ul>
+										</details>
+									</li>
+									<li>
+										<details>
+											<summary>ОСОС (Отдел Сервисного Обслуживания Сети)</summary>
+											<ul>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Как мне сделать так, чтобы друг смог зайти на мой сервер?',()=>{
+															window.open(ОСОС_link, '_blank');
+														});
+													}}>Запросить консультацию по предоставлению доступа к игровому серверу</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Как мне открыть порт?',()=>{
+															window.open(ОСОС_link, '_blank');
+														});
+													}}>Запросить консультацию по открытию порта</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Как мне создать виртуальную локальную сеть?',()=>{
+															window.open(ОСОС_link, '_blank');
+														});
+													}}>Запросить консультацию по созданию виртуальной локальной сети</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard(other,()=>{
+															window.open(ОСОС_link, '_blank');
+														});
+													}}>Обращение по другому поводу</button>
+												</li>
+											</ul>
+										</details>
+									</li>
+									<li>
+										<details>
+											<summary>ХВЗ (Хранилище Вечного Знания)</summary>
+											<ul>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Мне нужна книга.',()=>{
+															window.open(ХВЗ_link, '_blank');
+														});
+													}}>Запрос на предоставление доступа к книге</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Посоветуйте книгу.',()=>{
+															window.open(ХВЗ_link, '_blank');
+														});
+													}}>Рекомендация книги по запросу</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard('Посоветуйте случайную книгу.',()=>{
+															window.open(ХВЗ_link, '_blank');
+														});
+													}}>Рекомендация случайной книги</button>
+												</li>
+												<li>
+													<button onClick={()=>{
+														copyToClipboard(other,()=>{
+															window.open(ХВЗ_link, '_blank');
+														});
+													}}>Обращение по другому поводу</button>
+												</li>
+											</ul>
+										</details>
+									</li>
+								</ul>
+							</details>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="window" role="tabpanel" style={{ display: activeTab === 2 ? 'block' : 'none' }}>
+				<div className="window-body">
+					<ul className="tree-view">
+						<li>
+							<details open>
+								<summary>Список калькуляторов:</summary>
+								<ul>
+									<li>
+										<details>
+											<summary>Игры</summary>
+											<ul>
+												<li>
+													<details>
+														<summary>Crossout</summary>
+														<ul>
+															<li>
+																<button onClick={()=>
+																	openWindow({ title: 'Калькулятор разбора декора Crossout', children: <DecorDisassembling/> })
+																}>Разбор декора</button>
+															</li>
+														</ul>
+													</details>
+												</li>
+											</ul>
+										</details>
+									</li>
+								</ul>
+							</details>
+						</li>
 					</ul>
 				</div>
 			</div>
---
+++ src/components/calculators/crossout/DecorDisassembling.jsx
@@ -0,0 +1,7 @@
+export default function DecorDisassembling() {
+    return(
+        <>
+            
+        </>
+    )
+}
